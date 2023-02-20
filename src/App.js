82461b9d79aa7c0adbff96964ddbf63400b88969
Hoàng Oanh <duongngochoangoanh22810024@gmail.com>
2023-02-20 13:44:35 +0700

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute } from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+    return (
+        <Router>
+            <Navbar />
+            <Sidebar />
+            <Switch>
+                <Router exact path="/">
+                    <Home />
+                </Router>
+                <Router exact path="/about">
+                    <About />
+                </Router>
+                <Router exact path="/cart">
+                    <Cart />
+                </Router>
+                <Router exact path="/products">
+                    <Products />
+                </Router>
+
+                <Router exact path="/products/:id" children={<SingleProduct />} />
+                <Router exact path="/checkout">
+                    <Checkout />
+                </Router>
+
+                <Router path="*">
+                    <Error />
+                </Router>
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
-export default App
+export default App;
